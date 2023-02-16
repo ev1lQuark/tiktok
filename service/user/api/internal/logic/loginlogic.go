ef6959730e47f67ce2ea258a6474a2733b216d2c
WangGuan <wg1024code@gmail.com>
2023-02-16 10:58:01 +0800

    feat(user): 实现登录注册功能，CRUD代码生成器改为gorm-gen

--- service/user/api/internal/logic/loginlogic.go
+++ service/user/api/internal/logic/loginlogic.go
@@ -2,9 +2,15 @@ package logic
 
 import (
 	"context"
+	"strings"
+	"time"
 
+	"github.com/ev1lQuark/tiktok/common/encrypt"
+	"github.com/ev1lQuark/tiktok/common/jwt"
+	"github.com/ev1lQuark/tiktok/common/res"
 	"github.com/ev1lQuark/tiktok/service/user/api/internal/svc"
 	"github.com/ev1lQuark/tiktok/service/user/api/internal/types"
+	"gorm.io/gorm"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +30,37 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 }
 
 func (l *LoginLogic) Login(req *types.LoginOrRegisterReq) (resp *types.LoginOrRegisterReply, err error) {
-	// todo: add your logic here and delete this line
+	// 参数校验
+	if len(strings.TrimSpace(req.Username)) == 0 || len(strings.TrimSpace(req.Password)) == 0 {
+		resp = &types.LoginOrRegisterReply{Code: res.DefaultErrorCode, Message: "参数错误"}
+		return resp, nil
+	}
 
-	return
+	userQuery := l.svcCtx.Query.User
+	// 判断用户是否存在
+	user, err := userQuery.WithContext(context.TODO()).Where(userQuery.Name.Eq(req.Username)).Take()
+	if err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			resp = &types.LoginOrRegisterReply{Code: res.DefaultErrorCode, Message: "用户不存在"}
+			return resp, nil
+		default:
+			return nil, err
+		}
+	}
+
+	// 校验密码
+	p := encrypt.Sha256Encrypt(req.Password)
+	if user.Password != p {
+		resp = &types.LoginOrRegisterReply{Code: res.DefaultErrorCode, Message: "密码错误"}
+		return resp, nil
+	}
+
+	// 生成token
+	token, err := jwt.GetJwtToken(l.svcCtx.Config.Auth.AccessSecret, time.Now().Unix(), l.svcCtx.Config.Auth.AccessExpire, user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.LoginOrRegisterReply{Code: res.SuccessCode, Message: "登录成功", UserId: user.ID, Token: token}, nil
 }
--- service/user/api/internal/logic/registerlogic.go
+++ service/user/api/internal/logic/registerlogic.go
@@ -2,9 +2,16 @@ package logic
 
 import (
 	"context"
+	"strings"
+	"time"
 
+	"github.com/ev1lQuark/tiktok/common/encrypt"
+	"github.com/ev1lQuark/tiktok/common/jwt"
+	"github.com/ev1lQuark/tiktok/common/res"
 	"github.com/ev1lQuark/tiktok/service/user/api/internal/svc"
 	"github.com/ev1lQuark/tiktok/service/user/api/internal/types"
+	"github.com/ev1lQuark/tiktok/service/user/model"
+	"gorm.io/gorm"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,7 +31,43 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 }
 
 func (l *RegisterLogic) Register(req *types.LoginOrRegisterReq) (resp *types.LoginOrRegisterReply, err error) {
-	// todo: add your logic here and delete this line
+	// 参数校验
+	if len(strings.TrimSpace(req.Username)) == 0 || len(strings.TrimSpace(req.Password)) == 0 {
+		resp = &types.LoginOrRegisterReply{Code: res.DefaultErrorCode, Message: "参数错误"}
+		return resp, nil
+	}
 
-	return
+	userQuery := l.svcCtx.Query.User
+	// 判断用户是否存在
+	_, err = userQuery.WithContext(context.TODO()).Where(userQuery.Name.Eq(req.Username)).Take()
+	if err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			break
+		default:
+			return nil, err
+		}
+	} else {
+		resp = &types.LoginOrRegisterReply{Code: res.DefaultErrorCode, Message: "用户已存在"}
+		return resp, nil
+	}
+
+	// 创建用户
+	user := &model.User{
+		Name:     req.Username,
+		Password: encrypt.Sha256Encrypt(req.Password),
+	}
+	err = userQuery.WithContext(context.TODO()).Create(user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// 生成jwt
+	token, err := jwt.GetJwtToken(l.svcCtx.Config.Auth.AccessSecret, time.Now().Unix(), l.svcCtx.Config.Auth.AccessExpire, user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.LoginOrRegisterReply{Code: res.SuccessCode, Message: "注册成功", UserId: user.ID, Token: token}, nil
 }
