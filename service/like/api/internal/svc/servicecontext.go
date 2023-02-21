7706afa6da3afc425098052820cb48c855a23d31
SDUshuo <57951937+SDUshuo@users.noreply.github.com>
2023-02-21 12:12:12 +0800

    fix: getlkelistlogic更新 (#21)

--- service/like/api/internal/svc/servicecontext.go
+++ service/like/api/internal/svc/servicecontext.go
@@ -1,26 +1,31 @@
 package svc
 
 import (
+	"github.com/ev1lQuark/tiktok/common/db"
+	"github.com/ev1lQuark/tiktok/service/comment/rpc/commentclient"
 	"github.com/ev1lQuark/tiktok/service/like/api/internal/config"
 	"github.com/ev1lQuark/tiktok/service/like/query"
-	"gorm.io/driver/mysql"
+	"github.com/ev1lQuark/tiktok/service/user/rpc/userclient"
+	"github.com/ev1lQuark/tiktok/service/video/rpc/videoclient"
+	"github.com/zeromicro/go-zero/zrpc"
 	"gorm.io/gorm"
 )
 
 type ServiceContext struct {
-	Config config.Config
-	Query  *query.Query
+	Config     config.Config
+	Query      *query.Query
+	UserRpc    userclient.User
+	CommentRpc commentclient.Comment
+	VideoRpc   videoclient.Video
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
-	db, err := gorm.Open(mysql.Open(c.Mysql.DataSource), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	query := query.Use(db)
 
 	return &ServiceContext{
-		Config: c,
-		Query:  query,
+		Config:     c,
+		Query:      query.Use(db.NewMysqlConn(c.Mysql.DataSource, &gorm.Config{})),
+		UserRpc:    userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+		CommentRpc: commentclient.NewComment(zrpc.MustNewClient(c.CommentRpc)),
+		VideoRpc:   videoclient.NewVideo(zrpc.MustNewClient(c.VideoRpc)),
 	}
 }
