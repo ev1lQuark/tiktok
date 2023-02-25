0585208e83fe81af0826ea1d5fdf2a6857ce29a4
WangGuan <wg1024code@gmail.com>
2023-02-25 20:35:52 +0800

    refactor(user, video, comment): 更新其他service对于like的rpc调用

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ev1lQuark/tiktok
 go 1.19
 
 require (
+	github.com/apache/rocketmq-client-go/v2 v2.1.1
 	github.com/chilts/sid v0.0.0-20190607042430-660e94789ec9
 	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/minio/minio-go/v7 v7.0.49
@@ -18,8 +19,17 @@ require (
 
 require (
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/stathat/consistent v1.0.0 // indirect
+	github.com/tidwall/gjson v1.13.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
 
 require (
--- service/video/api/internal/logic/publishlistlogic.go
+++ service/video/api/internal/logic/publishlistlogic.go
@@ -62,13 +62,12 @@ func (l *PublishListLogic) PublishList(req *types.PublishListReq) (resp *types.P
 		resp = &types.PublishListReply{StatusCode: res.BadRequestCode, StatusMsg: "获取用户的视频发布列表失败"}
 		return resp, nil
 	}
-	authorIds := make([]int64, 0, len(tableVideos))
-	videoIds := make([]int64, 0, len(tableVideos))
 
+	videoIds := make([]int64, 0, len(tableVideos))
 	for _, value := range tableVideos {
-		authorIds = append(authorIds, value.AuthorID)
 		videoIds = append(videoIds, value.ID)
 	}
+	authorIds := []int64{userId}
 
 	var eg errgroup.Group
 
@@ -81,31 +80,30 @@ func (l *PublishListLogic) PublishList(req *types.PublishListReq) (resp *types.P
 	})
 
 	// 根据userId获取本账号获赞总数
-	var totalFavoriteNumList *like.GetTotalFavoriteNumReply
-
+	var totalFavoriteNumList *like.GetFavoriteCountByAuthorIdsReply
 	eg.Go(func() error {
 		var err error
-		totalFavoriteNumList, err = l.svcCtx.LikeRpc.GetTotalFavoriteNum(l.ctx, &like.GetTotalFavoriteNumReq{UserId: authorIds})
+		totalFavoriteNumList, err = l.svcCtx.LikeRpc.GetFavoriteCountByAuthorIds(l.ctx, &like.GetFavoriteCountByAuthorIdsReq{AuthorIds: authorIds})
 		return err
 	})
 
 	// 根据userId获取本账号喜欢（点赞）总数
-	var userFavoriteCountList *like.GetFavoriteCountByUserIdReply
+	var userFavoriteCountList *like.GetFavoriteCountByUserIdsReply
 	eg.Go(func() error {
 		var err error
-		userFavoriteCountList, err = l.svcCtx.LikeRpc.GetFavoriteCountByUserId(l.ctx, &like.GetFavoriteCountByUserIdReq{UserId: authorIds})
+		userFavoriteCountList, err = l.svcCtx.LikeRpc.GetFavoriteCountByUserIds(l.ctx, &like.GetFavoriteCountByUserIdsReq{UserIds: authorIds})
 		return err
 	})
 
-	// 根据videoId获取视屏点赞总数
-	var videoFavoriteCountList *like.GetFavoriteCountByVideoIdReply
+	// 根据videoId获取视频点赞总数
+	var videoFavoriteCountList *like.GetFavoriteCountByVideoIdsReply
 	eg.Go(func() error {
 		var err error
-		videoFavoriteCountList, err = l.svcCtx.LikeRpc.GetFavoriteCountByVideoId(l.ctx, &like.GetFavoriteCountByVideoIdReq{VideoId: videoIds})
+		videoFavoriteCountList, err = l.svcCtx.LikeRpc.GetFavoriteCountByVideoIds(l.ctx, &like.GetFavoriteCountByVideoIdsReq{VideoIds: videoIds})
 		return err
 	})
 
-	// 根据videoId获取视屏评论总数
+	// 根据videoId获取视频评论总数
 	var videoCommentCountList *comment.GetComentCountByVideoIdReply
 	eg.Go(func() error {
 		var err error
@@ -117,11 +115,15 @@ func (l *PublishListLogic) PublishList(req *types.PublishListReq) (resp *types.P
 	var isFavoriteList = &like.IsFavoriteReply{}
 	eg.Go(func() error {
 		var err error
-		isFavoriteList, err = l.svcCtx.LikeRpc.IsFavorite(l.ctx, &like.IsFavoriteReq{VideoId: videoIds, UserId: authorIds})
+		authorIds := make([]int64, 0, len(tableVideos))
+		for range tableVideos {
+			authorIds = append(authorIds, userId)
+		}
+		isFavoriteList, err = l.svcCtx.LikeRpc.IsFavorite(l.ctx, &like.IsFavoriteReq{VideoIds: videoIds, UserIds: authorIds})
 		return err
 	})
 
-	//错误判断
+	// errorgroup 等待所有请求完成 错误处理
 	if err := eg.Wait(); err != nil {
 		msg := fmt.Sprintf("調用Rpc失敗%v", err)
 		logx.Error(msg)
@@ -130,44 +132,37 @@ func (l *PublishListLogic) PublishList(req *types.PublishListReq) (resp *types.P
 	}
 
 	// 获取workCount
-	workCount := make([]int, 0, len(tableVideos))
-	for index := 0; index < len(tableVideos); index++ {
-		count, err := videoQuery.WithContext(context.TODO()).Where(videoQuery.AuthorID.Eq(authorIds[index])).Count()
-		if err != nil {
-			msg := fmt.Sprintf("查询视频失败：%v", err)
-			logx.Error(msg)
-			resp = &types.PublishListReply{StatusCode: res.BadRequestCode, StatusMsg: msg}
-			return resp, nil
-		}
-		workCount = append(workCount, int(count))
+	workCount, err := videoQuery.WithContext(context.TODO()).Where(videoQuery.AuthorID.Eq(userId)).Count()
+	if err != nil {
+		msg := fmt.Sprintf("查询视频失败：%v", err)
+		logx.Error(msg)
+		resp = &types.PublishListReply{StatusCode: res.BadRequestCode, StatusMsg: msg}
+		return resp, nil
 	}
 
 	// 拼接请求
-	fmt.Println(len(userNameList.NameList))
-	fmt.Println(len(totalFavoriteNumList.Count))
-	fmt.Println(len(userFavoriteCountList.Count))
 	videos := make([]types.VideoList, 0, len(tableVideos))
 	for index, value := range tableVideos {
 		videos = append(videos, types.VideoList{
 			ID: int(value.ID),
 			Author: types.Author{
-				ID:              int(authorIds[index]),
-				Name:            userNameList.NameList[index],
+				ID:              int(userId),
+				Name:            userNameList.NameList[0],
 				FollowCount:     0,
 				FollowerCount:   0,
 				IsFollow:        false,
 				Avatar:          "https://inews.gtimg.com/newsapp_bt/0/13352207849/1000",
 				BackgroundImage: "https://inews.gtimg.com/newsapp_bt/0/13352207849/1000",
-				Signature:       "愛抖音，爱生活",
-				TotalFavorited:  strconv.Itoa(int(totalFavoriteNumList.Count[index])),
-				WorkCount:       workCount[index],
-				FavoriteCount:   int(userFavoriteCountList.Count[index]),
+				Signature:       "爱抖音，爱生活",
+				TotalFavorited:  strconv.FormatInt(totalFavoriteNumList.CountSlice[0], 10),
+				WorkCount:       int(workCount),
+				FavoriteCount:   int(userFavoriteCountList.CountSlice[0]),
 			},
 			PlayURL:       "http://" + path.Join(l.svcCtx.Config.Minio.Endpoint, value.PlayURL),
 			CoverURL:      "http://" + path.Join(l.svcCtx.Config.Minio.Endpoint, value.CoverURL),
-			FavoriteCount: int(videoFavoriteCountList.Count[index]),
+			FavoriteCount: int(videoFavoriteCountList.CountSlice[0]),
 			CommentCount:  int(videoCommentCountList.Count[index]),
-			IsFavorite:    isFavoriteList.IsFavorite[index],
+			IsFavorite:    isFavoriteList.IsFavoriteSlice[index],
 			Title:         value.Title,
 		})
 	}
